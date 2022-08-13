fa3a6bf2e3778b7c89918099b5d068ed76f5b452
Febro Herdyanto <febroherdyanto@outloook.com>
2022-08-13 13:14:38 +0700

    Add Dashboard Menu

--- app/Views/content.php
+++ app/Views/content.php
@@ -14,20 +14,95 @@
                                                         <div class="row align-items-center">
                                                             <div class="col-8">
                                                                 <h4 class="text-c-purple">
-                                                                    lorem
+                                                                    <?php $db      = \Config\Database::connect();
+                                                                    $db = db_connect();
+                                                                    
+                                                                    $qjmltau = $db->query("select count(*) as idGuest from guest");
+                                                                    $tamu = $qjmltau->getResultArray();
+                                                                    echo $tamu[0]['idGuest']; ?> Orang
                                                                     
                                                                 </h4>
-                                                                <h6 class="text-muted m-b-0">Total Uang Kas</h6>
+                                                                <h6 class="text-muted m-b-0">Total Tamu Undangan</h6>
                                                             </div>
                                                             <div class="col-4 text-right">
-                                                                <i class="fa-solid fa-3x fa-file-invoice-dollar"></i>
+                                                                <i class="fa-solid fa-3x fa-people-roof"></i>
                                                             </div>
                                                         </div>
                                                     </div>
                                                     <div class="card-footer bg-c-purple">
                                                         <div class="row align-items-center">
                                                             <div class="col-9">
-                                                                <p class="text-white m-b-0"><i>Updated data.</i></p>
+                                                                <p class="text-white m-b-0"><i>Updated data on  <?php echo date('d - M - Y') ?>.</i></p>
+                                                            </div>
+                                                            <div class="col-3 text-right">
+                                                                <i class="fa fa-line-chart text-white f-16"></i>
+                                                            </div>
+                                                        </div>
+            
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            
+                                            <div class="col-xl-3 col-md-6">
+                                                <div class="card">
+                                                    <div class="card-block">
+                                                        <div class="row align-items-center">
+                                                            <div class="col-8">
+                                                                <h4 class="text-c-red">
+                                                                    <?php $db      = \Config\Database::connect();
+                                                                    $db = db_connect();
+                                                                    
+                                                                    $qjmltau = $db->query("select count(*) as idGuest from guest where idKategori = 1");
+                                                                    $tamu = $qjmltau->getResultArray();
+                                                                    echo $tamu[0]['idGuest']; ?> Orang
+                                                                    
+                                                                </h4>
+                                                                <h6 class="text-muted m-b-0">Tamu Undangan Pria</h6>
+                                                            </div>
+                                                            <div class="col-4 text-right">
+                                                                <i class="fa-solid fa-3x fa-people-roof"></i>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="card-footer bg-c-blue">
+                                                        <div class="row align-items-center">
+                                                            <div class="col-9">
+                                                                <p class="text-white m-b-0"><i>Updated data on  <?php echo date('d - M - Y') ?>.</i></p>
+                                                            </div>
+                                                            <div class="col-3 text-right">
+                                                                <i class="fa fa-line-chart text-white f-16"></i>
+                                                            </div>
+                                                        </div>
+            
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            
+                                            <div class="col-xl-3 col-md-6">
+                                                <div class="card">
+                                                    <div class="card-block">
+                                                        <div class="row align-items-center">
+                                                            <div class="col-8">
+                                                                <h4 class="text-c-red">
+                                                                    <?php $db      = \Config\Database::connect();
+                                                                    $db = db_connect();
+                                                                    
+                                                                    $qjmltau = $db->query("select count(*) as idGuest from guest where idKategori = 2");
+                                                                    $tamu = $qjmltau->getResultArray();
+                                                                    echo $tamu[0]['idGuest']; ?> Orang
+                                                                    
+                                                                </h4>
+                                                                <h6 class="text-muted m-b-0">Tamu Undangan Wanita</h6>
+                                                            </div>
+                                                            <div class="col-4 text-right">
+                                                                <i class="fa-solid fa-3x fa-people-roof"></i>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="card-footer bg-c-blue">
+                                                        <div class="row align-items-center">
+                                                            <div class="col-9">
+                                                                <p class="text-white m-b-0"><i>Updated data on  <?php echo date('d - M - Y') ?>.</i></p>
                                                             </div>
                                                             <div class="col-3 text-right">
                                                                 <i class="fa fa-line-chart text-white f-16"></i>
